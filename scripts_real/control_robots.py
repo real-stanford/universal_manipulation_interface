c81fe958553697118a6d3433375b4bf9c1c6b455
zhangheng <zhanghengsjtu@gmail.com>
2024-04-16 18:00:23 +0800

    modify scripts_real/control_robots.py to test real world robot

--- scripts_real/control_robots.py
+++ scripts_real/control_robots.py
@@ -13,51 +13,44 @@ import time
 import numpy as np
 from multiprocessing.managers import SharedMemoryManager
 import scipy.spatial.transform as st
-from umi.real_world.spacemouse_shared_memory import Spacemouse
 from umi.real_world.rtde_interpolation_controller import RTDEInterpolationController
 from umi.real_world.dh_controller import DHController
 from umi.common.precise_sleep import precise_wait
 
 # %%
 @click.command()
-@click.option('-rh', '--robot_hostname', default='172.24.95.9')
-@click.option('-gh', '--gripper_hostname', default='172.24.95.27')
-@click.option('-gp', '--gripper_port', type=int, default=1000)
+@click.option('-rh', '--robot_hostname', default='192.168.2.152')
+# @click.option('-gh', '--gripper_hostname', default='172.24.95.27')
+# @click.option('-gp', '--gripper_port', type=int, default=1000)
 @click.option('-f', '--frequency', type=float, default=30)
-@click.option('-gs', '--gripper_speed', type=float, default=200.0)
-def main(robot_hostname, gripper_hostname, gripper_port, frequency, gripper_speed):
+@click.option('-gs', '--gripper_speed', type=float, default=0.07273)
+def main(robot_hostname, frequency, gripper_speed):
     max_pos_speed = 0.25
     max_rot_speed = 0.6
-    max_gripper_width = 90.
+    max_gripper_width = 0.08
     cube_diag = np.linalg.norm([1,1,1])
     tcp_offset = 0.13
-    # tcp_offset = 0
     dt = 1/frequency
     command_latency = dt / 2
 
     with SharedMemoryManager() as shm_manager:
         with DHController(
             shm_manager=shm_manager,
-            hostname=gripper_hostname,
-            port=gripper_port,
-            frequency=frequency,
-            move_max_speed=400.0,
-            verbose=False
+            port="/dev/ttyUSBDH_",
+            receive_latency=0.01,
+            use_meters=True
         ) as gripper,\
         RTDEInterpolationController(
             shm_manager=shm_manager,
             robot_ip=robot_hostname,
             frequency=125,
-            lookahead_time=0.05,
-            gain=1000,
+            lookahead_time=0.1,
+            gain=300,
             max_pos_speed=max_pos_speed*cube_diag,
             max_rot_speed=max_rot_speed*cube_diag,
             tcp_offset_pose=[0,0,tcp_offset,0,0,0],
             verbose=False
-        ) as controller,\
-        Spacemouse(
-            shm_manager=shm_manager
-        ) as sm:
+        ) as controller:
             print('Ready!')
             # to account for recever interfance latency, use target pose
             # to init buffer.
@@ -67,16 +60,22 @@ def main(robot_hostname, gripper_hostname, gripper_port, frequency, gripper_spee
             t_start = time.monotonic()
             gripper.restart_put(t_start-time.monotonic() + time.time())
             
+            ur_start_time = time.time()
+            dh_start_time = time.time()
             iter_idx = 0
             while True:
-                s = time.time()
                 t_cycle_end = t_start + (iter_idx + 1) * dt
                 t_sample = t_cycle_end - command_latency
                 t_command_target = t_cycle_end + dt
 
                 precise_wait(t_sample)
-                sm_state = sm.get_motion_state_transformed()
-                # print(sm_state)
+                sm_state = [0, 0, 0, 0, 0, 0]
+                if time.time() - ur_start_time > 5:
+                    sm_state[2] = 1
+                elif time.time() - ur_start_time > 10:
+                    sm_state[2] = -1
+                    ur_start_time = time.time()
+
                 dpos = sm_state[:3] * (max_pos_speed / frequency)
                 drot_xyz = sm_state[3:] * (max_rot_speed / frequency)
 
@@ -86,11 +85,11 @@ def main(robot_hostname, gripper_hostname, gripper_port, frequency, gripper_spee
                     target_pose[3:])).as_rotvec()
                 
                 dpos = 0
-                if sm.is_button_pressed(0):
-                    # close gripper
+                if time.time() - dh_start_time > 3:
                     dpos = -gripper_speed / frequency
-                if sm.is_button_pressed(1):
+                elif time.time() - dh_start_time > 6:
                     dpos = gripper_speed / frequency
+                    dh_start_time = time.time()
                 gripper_target_pos = np.clip(gripper_target_pos + dpos, 0, max_gripper_width)
  
                 controller.schedule_waypoint(target_pose, 
