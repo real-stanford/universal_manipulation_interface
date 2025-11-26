d0b29386a12aa0bbcccaffacbad9540d4d10c680
sungjoon-hwang <sjhwang0825@gmail.com>
2025-11-26 14:57:06 +0900

    test

--- helper/marker_test.sh
+++ helper/marker_test.sh
@@ -4,8 +4,6 @@
 #  --intrinsics_json ~/Desktop/study/universal_manipulation_interface/example/calibration/realsense_intrinsics.json\
 #  --aruco_yaml ~/Desktop/study/universal_manipulation_interface/example/calibration/aruco_config.yaml
 
-
-
 python3 ~/Desktop/study/universal_manipulation_interface/scripts/detect_aruco.py\
  --input /home/sungjoon/Desktop/study/universal_manipulation_interface/my_example_demo_session/demos/gripper_calibration_135122070988_2025.11.24_19.00.18.856155/raw_video.mp4\
  --output ~/Documents/tag_detection.pkl\
